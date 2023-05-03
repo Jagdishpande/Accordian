a92cb34a250d65961562448e145b5855f79c5837
Jagdish Pande <122719486+8668577812@users.noreply.github.com>
2023-05-03 18:57:47 +0530

    work on accordian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccordianComponent } from './shared/component/accordian/accordian.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordianComponent
   ],
   imports: [
     BrowserModule
